187642113cdc4c0dd0379977dbad588c9821e5d3
Juan Manuel Mougan <juanmougan@gmail.com>
2020-08-07 19:48:06 -0300

    Fullstack version using vanilla JS

--- api/api.rb
+++ api/api.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
-  visitors = 0
-  get '/visitors' do
-  	visitors = visitors + 1
-  	"#{visitors}"
-  end
+require 'sinatra/cross_origin'
+
+# Configure CORS
+set :bind, '0.0.0.0'
+
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
+end
+# End CORS
+
+visitors = 0
+
+get '/visitors' do
+  visitors = visitors + 1
+  "#{visitors}"
+end
