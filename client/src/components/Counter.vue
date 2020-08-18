bff7ca9940fbdab7e6ca32afa9f0a1b4584a7250
Juan Manuel Mougan <juanmougan@gmail.com>
2020-08-17 23:10:12 -0300

    Cleaned up code

--- client/src/components/Counter.vue
+++ client/src/components/Counter.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+// TODO use the Service instead
 // import Visitors from "@/services/visitors.js";
 const axios = require("axios");
 
@@ -19,28 +20,6 @@ export default {
       visitors: 0,
     };
   },
-  // methods: {
-  //   // async getVisitorsCount() {
-  //   //   Visitors.getVisitors()
-  //   //     .then((count) => (this.visitors = count))
-  //   //     .catch((err) => {
-  //   //       console.log("Something went wrong: ", err);
-  //   //     });
-  //   // },
-  //   promiseBasedGet: function () {
-  //     axios
-  //       .get("http://localhost:5100/visitors")
-  //       .then(function (response) {
-  //         // handle success
-  //         console.log(`Got this: ${response.data}`);
-  //         this.visitors = response.data;
-  //       })
-  //       .catch(function (error) {
-  //         // handle error
-  //         console.log(error);
-  //       });
-  //   },
-  // },
   created() {
     axios
       .get(`http://localhost:5100/visitors`)
@@ -49,18 +28,8 @@ export default {
         this.visitors = response.data;
       })
       .catch((e) => {
-        // handle error
         console.log(e);
       });
-
-    // async / await version (created() becomes async created())
-    //
-    // try {
-    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    //   this.posts = response.data
-    // } catch (e) {
-    //   this.errors.push(e)
-    // }
   },
 };
 </script>
